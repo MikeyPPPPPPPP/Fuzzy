7f71740569c0a646cdb4a0f0dcfea4ee83678da6
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2022-09-28 20:59:42 -0700

    Update formatRequest.py

--- core/formatRequest.py
+++ core/formatRequest.py
@@ -9,26 +9,48 @@ class requestHandler(object):
         #this will be the word we are fuzzing with
         self.settings = settings
         self.fuzzItem = fuzzItem
-        if postOrGet(settings['postData']):
-            packet = self.__getRequest(self.settings, self.fuzzItem)
+
+        if self.settings["tor"]:
+            self.session = requests.session()
+            self.session.proxies = {}
+            self.session.proxies['http'] = 'socks5h://127.0.0.1:9150'
+            self.session.proxies['https'] = 'socks5h://127.0.0.1:9150'
+
+        if postOrGet(self.settings['postData']):
+            packet = self.__getRequest(self.fuzzItem)
             return packet.status_code, packet.text
 
-        packet = self.__postRequest(self.settings, self.fuzzItem)
+        packet = self.__postRequest(self.fuzzItem)
         return packet.status_code, packet.text
 
-    def __getRequest(self, settings, fuzzItem):
+    def __getRequest(self, fuzzItem):
         '''returns a GET request'''
-        url = settings['url'].replace('FUZZ', fuzzItem)
-        return requests.get(url, headers=settings['headers'], allow_redirects=False)
+        url = self.settings['url'].replace('FUZZ', fuzzItem)
+        if self.settings["followRedirects"] == 'true':#if redirects true
+            if self.settings["tor"]:#                   if tor
+                return self.session.get(url, headers=self.settings['headers'], allow_redirects=True)
+            return requests.get(url, headers=self.settings['headers'], allow_redirects=True)
+
+        if self.settings["tor"]:
+            return self.session.get(url, headers=self.settings['headers'], allow_redirects=False)
+        return requests.get(url, headers=self.settings['headers'], allow_redirects=False)
         
-    def __postRequest(self, settings, fuzzItem):
+    def __postRequest(self, fuzzItem):
         '''returns a POST request'''
-        url = settings['url'].replace('FUZZ', fuzzItem)
-        return requests.post(url, headers=settings['headers'], data=settings['postData'], allow_redirects=False)
+        url = self.settings['url'].replace('FUZZ', fuzzItem)
+        if self.settings["followRedirects"] == 'true':#if redirects true
+            if self.settings["tor"]:#                   if tor
+                return self.session.post(url, headers=self.settings['headers'], data=self.settings['postData'], allow_redirects=True)
+            return requests.post(url, headers=self.settings['headers'], data=self.settings['postData'], allow_redirects=True)
+
+        if self.settings["tor"]:
+            return self.session.post(url, headers=self.settings['headers'], data=self.settings['postData'], allow_redirects=False)
+        return requests.post(url, headers=self.settings['headers'], data=self.settings['postData'], allow_redirects=False)
 
 def postOrGet(inData) -> bool:
     '''private method, true = get, false = post '''
     if inData == "":
         return True
     return False
-        
+
+
