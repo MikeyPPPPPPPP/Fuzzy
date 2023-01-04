c8418336bffa82073be5903c5a2b8fed8f9c4e57
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2023-01-03 18:45:48 -0800

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,4 @@
+from .getArguments import parseStatusCodes, parseCharectorCount, parseWordCount, getArgs
+from .setSettings import setup
+from .spammer import bombsAway
+from .graphics import banner
