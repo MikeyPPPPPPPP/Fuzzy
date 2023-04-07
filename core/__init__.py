90eef528130c9325f4ff88d8bbcaebb4ff9142e9
Michael Provenzano <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2023-04-07 12:52:49 -0700

    Delete core directory

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .getArguments import parseStatusCodes, parseCharectorCount, parseWordCount, parseResponseCode, getArgs
-from .setSettings import setup
-from .spammer import bombsAway
-from .graphics import banner
-
--- core/tumbler/__init__.py
+++
@@ -1,1 +0,0 @@
-from .userAgent_tumbler import userAgent_tumbler
